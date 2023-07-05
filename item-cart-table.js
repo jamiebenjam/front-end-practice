ea8fbade00648a3198a58cd545b68c2a4411852b
Jamie Benjamin <bennythedev@gmail.com>
2023-07-04 21:45:01 -0500

    item cart table

---
+++ item-cart-table.js
@@ -0,0 +1,34 @@
+<table>
+  <caption>
+    Item Cart
+  </caption>
+  <thead>
+    <tr>
+      <th scope="col">Item</th>
+      <th scope="col">Price</th>
+      <th scope="col">Quantity</th>
+    </tr>
+  </thead>
+  <tr>
+    <th scope="row">Banana</th>
+    <td>$1.00</td>
+    <td>3</td>
+  </tr>
+  <tr>
+    <th scope="row">Burger</th>
+    <td>$6.90</td>
+    <td>1</td>
+  </tr>
+  <tr>
+    <th scope="row">Cookie</th>
+    <td>$4.20</td>
+    <td>4</td>
+  </tr>
+  <tfoot>
+    <tr>
+      <th>Total</th>
+      <td>$12.10</td>
+      <td>8</td>
+    </tr>
+  </tfoot>
+</table>
