8b43bd9c1441cce3b8a0d628a2f41bafab1f0c0f
Jamie Benjamin <bennythedev@gmail.com>
2023-07-08 17:40:29 -0500

    rainbow circles css

---
+++ rainbow-circles-css.js
@@ -0,0 +1,29 @@
+/*  Write your code here. */
+
+div {
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+#outer {
+  height: 250px;
+  width: 250px;
+  border: 25px solid #ff0000;
+  background-color: #ffa500;
+}
+
+#middle {
+  height: 150px;
+  width: 150px;
+  border: 25px solid #ffff00;
+  background-color: #008000;
+}
+
+#inner {
+  height: 50px;
+  width: 50px;
+  border: 25px solid #0000ff;
+  background-color: #800080
+}
